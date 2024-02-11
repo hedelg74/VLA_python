fc3a85a26fe7f666a9f26a32d6ec1dfcd00787c9
Henry Delgado <hdelgados74@gmail.com>
2024-02-11 10:49:26 -0600

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,17 @@
+"""
+Ejercico #2.
+
+Elabore un programa que solicite al usuario la edad y calcule cuantos años
+tendra la persona en 5 años.
+Al finaliza mprime el siguiente mensaje "Dentro de 5 años tendra: " y se le
+muestra el valor de la edad proyectada.
+
+"""
+
+edadActual=int(input("Digite su edad: "))
+edadProyectada=edadActual + 5
+
+print("Dentro de 5 años, tendra: ", edadProyectada, "años")
+
+
+
