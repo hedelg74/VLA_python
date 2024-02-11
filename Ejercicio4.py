06cf5e69742808d7805d153b7afa139baa250494
Henry Delgado <hdelgados74@gmail.com>
2024-02-11 11:22:00 -0600

    Add files via upload

---
+++ Ejercicio4.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio #4.
+
+Desarrole un programa que le solicite un numero al usuario
+y calcule el cuadrado y el cubo de este."""
+
+numero=int(input("Digite un numero: "))
+
+cuadrado=numero **2
+cubo=numero **3
+
+print(f"\nEl cuadrado de:  {numero} es {cuadrado}")
+print(f"El cubo de:      {numero} es {cubo}\n")
+      
