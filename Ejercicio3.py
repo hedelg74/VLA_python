9f03b8b6b3873dbe068b5142b3df337bc834f4cc
Henry Delgado <hdelgados74@gmail.com>
2024-02-11 11:08:00 -0600

    Add files via upload

---
+++ Ejercicio3.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio #3.
+
+Construya un programa que solicite al usuario los datos enteros A y B
+y muestre el resultado de la siguiente expresion (A+B)2/3.
+
+"""
+
+A=int(input("Digite el valor de A: "))
+B=int(input("Digite el valor de B: "))
+
+resultado=(A+B)**2/3
+
+print("\nEl resultado de (A+B)^2/3 es:", resultado)
