9568f7ca7ca6b664b25d6e1573d7e4cfc9c8b490
Henry Delgado <hdelgados74@gmail.com>
2024-02-11 10:39:29 -0600

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,10 +2,9 @@
 Ejercicio #1.
 
 Construya un programa tal que, dados los datos para las
-variables A,B,C Y D, que representan numeros enteros,
+variables A,B,C y D, que representan numeros enteros,
 los imprima en orden inverso"""
 
 A=1; B=2; C=3; D=4
-
 
 print("Impresion al inverso:", D,C,B,A)
